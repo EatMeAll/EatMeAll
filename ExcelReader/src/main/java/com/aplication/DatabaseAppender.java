9de7007d67b9424e4d68f9682349c1caba1b5502
Igor <sowinski.igor@gmail.com>
2018-11-17 20:56:23 +0100

    add missing TypeMealRepository to fasade

--- ExcelReader/src/main/java/com/aplication/DatabaseAppender.java
+++ ExcelReader/src/main/java/com/aplication/DatabaseAppender.java
@@ -1,123 +1,123 @@
-package com.aplication;
-
-import com.WildBirds.RepositoryJPA.application.RepositoryFacade;
-import com.WildBirds.RepositoryJPA.domain.model.*;
-import com.WildBirds.RepositoryJPA.domain.model.enums.Language;
-import com.WildBirds.RepositoryJPA.domain.model.enums.MealTime;
-import com.domain.model.MealExcel;
-import com.domain.model.ProductsExcel;
-import com.domain.ports.ExcelReader;
-import com.infrastructure.XmlReader;
-
-
-import javax.ejb.EJB;
-import java.io.IOException;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-public class DatabaseAppender {
-
-//    @EJB
-//    RepositoryFacade repo;
+//package com.aplication;
 //
-//    public void addToDatabase() throws IOException{
-//
-//        ExcelReader excelReader = new XmlReader();
-//        String filePath = "ExcelReader\\src\\main\\resource\\jadlospis2.xlsx";
-//        List<MealExcel> mealExcelList = excelReader.read(filePath);
-//
-//        for (MealExcel mealExcel : mealExcelList) {
-//
-//            Meal mealEntity = new Meal();
-//
-//            String typeMealExcel = mealExcel.getTypeMeal();
-//            Integer prepairTimeExcel = mealExcel.getPrepaidTime();
-//            String titleExcel = mealExcel.getTitle();
-//            List<ProductsExcel> productsExcels = mealExcel.getProducts();
-//            String receiptExcel = mealExcel.getReceipt();
-//            Double caloriesExcel = mealExcel.getCalories();
-//            String authorExcel = mealExcel.getAuthor();
+//import com.WildBirds.RepositoryJPA.application.RepositoryFacade;
+//import com.WildBirds.RepositoryJPA.domain.model.*;
+//import com.WildBirds.RepositoryJPA.domain.model.enums.Language;
+//import com.WildBirds.RepositoryJPA.domain.model.enums.MealTime;
+//import com.domain.model.MealExcel;
+//import com.domain.model.ProductsExcel;
+//import com.domain.ports.ExcelReader;
+//import com.infrastructure.XmlReader;
 //
 //
-//            TypeMeal typeMealEntity = new TypeMeal();
+//import javax.ejb.EJB;
+//import java.io.IOException;
+//import java.time.Instant;
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//import java.util.Set;
 //
+//public class DatabaseAppender {
 //
-//            switch (typeMealExcel) {
-//
-//                case "sniadanie":
-//
-//                    typeMealEntity.setMealTime(MealTime.BREAKFAST);
-//                    break;
-//
-//                case "lunch":
-//
-//                    typeMealEntity.setMealTime(MealTime.LUNCH);
-//                    break;
-//
-//                case "obiad":
-//
-//                    typeMealEntity.setMealTime(MealTime.DINNER);
-//                    break;
-//
-//                case "kolacja":
-//
-//                    typeMealEntity.setMealTime(MealTime.SUPPER);
-//                    break;
-//            }
-//            repo.TYPEMEAL().insert(typeMealEntity);
-//
-//
-//            for (ProductsExcel productsExcel : productsExcels) {
-//                MealHasProduct mealHasProduct = new MealHasProduct();
-//                Product product = new Product();
-//
-//                String productsExcelName = productsExcel.getName();
-//                Double productsExcelAmount = productsExcel.getAmount();
-//                String productsExcelUnit = productsExcel.getUnit();
-//                String productsExcelSpecialUnit = productsExcel.getSpecialUnit();
-//
-//                product.setName(productsExcelName);
-//
-//                mealHasProduct.setAmount((int) Math.round(productsExcelAmount));
-//                mealHasProduct.setUnit(productsExcelUnit);
-//                mealHasProduct.setSpecialUnit(productsExcelSpecialUnit);
-//                mealHasProduct.setProduct(product);
-//                repo.PRODUCT().insert(product);
-//                repo.MEALHASPRODUCT().insert(mealHasProduct);
-//                mealEntity.getMealHasProductSet().add(mealHasProduct);
-//            }
-//
-//            Receipt receipt = new Receipt();
-//
-//            Step step = new Step();
-//            step.setHeader(receiptExcel);
-//            step.setNumber(1);
-//            repo.STEP().insert(step);
-//
-//            receipt.setDescription("Jakis dodatkowy opis");
-//            receipt.setTitle(titleExcel);
-//            receipt.setPrepareTime(prepairTimeExcel);
-//            receipt.getStepSet().add(step);
-//            repo.RECEIPT().insert(receipt);
-//
-//
-//
-//            mealEntity.setLanguage(Language.PL);
-//            mealEntity.setTitle(titleExcel);
-//            mealEntity.setShortDescription("Przepis z Excel");
-//            mealEntity.setAmountCalories((int) Math.round(caloriesExcel));
-//            mealEntity.setPublic(true);
-//            mealEntity.setCreatedDate(Instant.now());
-//            mealEntity.getTypeMeal().add(typeMealEntity);
-//            mealEntity.setReceipt(receipt);
-//            mealEntity.setAuthorReceipt(authorExcel);
-//
-//
-//        }
-//
-//
-//    }
-}
+////    @EJB
+////    RepositoryFacade repo;
+////
+////    public void addToDatabase() throws IOException{
+////
+////        ExcelReader excelReader = new XmlReader();
+////        String filePath = "ExcelReader\\src\\main\\resource\\jadlospis2.xlsx";
+////        List<MealExcel> mealExcelList = excelReader.read(filePath);
+////
+////        for (MealExcel mealExcel : mealExcelList) {
+////
+////            Meal mealEntity = new Meal();
+////
+////            String typeMealExcel = mealExcel.getTypeMeal();
+////            Integer prepairTimeExcel = mealExcel.getPrepaidTime();
+////            String titleExcel = mealExcel.getTitle();
+////            List<ProductsExcel> productsExcels = mealExcel.getProducts();
+////            String receiptExcel = mealExcel.getReceipt();
+////            Double caloriesExcel = mealExcel.getCalories();
+////            String authorExcel = mealExcel.getAuthor();
+////
+////
+////            TypeMeal typeMealEntity = new TypeMeal();
+////
+////
+////            switch (typeMealExcel) {
+////
+////                case "sniadanie":
+////
+////                    typeMealEntity.setMealTime(MealTime.BREAKFAST);
+////                    break;
+////
+////                case "lunch":
+////
+////                    typeMealEntity.setMealTime(MealTime.LUNCH);
+////                    break;
+////
+////                case "obiad":
+////
+////                    typeMealEntity.setMealTime(MealTime.DINNER);
+////                    break;
+////
+////                case "kolacja":
+////
+////                    typeMealEntity.setMealTime(MealTime.SUPPER);
+////                    break;
+////            }
+////            repo.TYPEMEAL().insert(typeMealEntity);
+////
+////
+////            for (ProductsExcel productsExcel : productsExcels) {
+////                MealHasProduct mealHasProduct = new MealHasProduct();
+////                Product product = new Product();
+////
+////                String productsExcelName = productsExcel.getName();
+////                Double productsExcelAmount = productsExcel.getAmount();
+////                String productsExcelUnit = productsExcel.getUnit();
+////                String productsExcelSpecialUnit = productsExcel.getSpecialUnit();
+////
+////                product.setName(productsExcelName);
+////
+////                mealHasProduct.setAmount((int) Math.round(productsExcelAmount));
+////                mealHasProduct.setUnit(productsExcelUnit);
+////                mealHasProduct.setSpecialUnit(productsExcelSpecialUnit);
+////                mealHasProduct.setProduct(product);
+////                repo.PRODUCT().insert(product);
+////                repo.MEALHASPRODUCT().insert(mealHasProduct);
+////                mealEntity.getMealHasProductSet().add(mealHasProduct);
+////            }
+////
+////            Receipt receipt = new Receipt();
+////
+////            Step step = new Step();
+////            step.setHeader(receiptExcel);
+////            step.setNumber(1);
+////            repo.STEP().insert(step);
+////
+////            receipt.setDescription("Jakis dodatkowy opis");
+////            receipt.setTitle(titleExcel);
+////            receipt.setPrepareTime(prepairTimeExcel);
+////            receipt.getStepSet().add(step);
+////            repo.RECEIPT().insert(receipt);
+////
+////
+////
+////            mealEntity.setLanguage(Language.PL);
+////            mealEntity.setTitle(titleExcel);
+////            mealEntity.setShortDescription("Przepis z Excel");
+////            mealEntity.setAmountCalories((int) Math.round(caloriesExcel));
+////            mealEntity.setPublic(true);
+////            mealEntity.setCreatedDate(Instant.now());
+////            mealEntity.getTypeMeal().add(typeMealEntity);
+////            mealEntity.setReceipt(receipt);
+////            mealEntity.setAuthorReceipt(authorExcel);
+////
+////
+////        }
+////
+////
+////    }
+//}
--- RepositoryJPA/src/main/java/com/WildBirds/RepositoryJPA/application/RepositoryFacade.java
+++ RepositoryJPA/src/main/java/com/WildBirds/RepositoryJPA/application/RepositoryFacade.java
@@ -59,6 +59,10 @@ public class RepositoryFacade {
         return this.STEP;
     }
 
+    public RepositoryTypeMeal TYPEMEAL(){
+        return this.TYPEMEAL;
+    }
+
     public RepositoryUser USER() {
         return this.USER;
     }
