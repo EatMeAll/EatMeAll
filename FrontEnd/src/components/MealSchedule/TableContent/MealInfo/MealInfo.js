83ff79be7b00e6321444d1a9a52536155d90e745
dianawieczorek <wieczorek.diana@outlook.com>
2019-01-08 14:44:47 +0100

    graphic hover buttons in DT and mobile

--- FrontEnd/src/components/MealSchedule/TableContent/MealInfo/MealInfo.js
+++ FrontEnd/src/components/MealSchedule/TableContent/MealInfo/MealInfo.js
@@ -2,12 +2,24 @@ import React, {Component} from 'react';
 
 import styles from './MealInfo.css';
 
+
 class MealInfo extends Component {
-    render () {
+
+
+    render() {
         return (
-            <div className={styles.MealInfo}>
-                <div className={styles.TypeOfMeal}>{this.props.mealType}</div>
-                <div> {this.props.mealName}</div>
+            <div className={styles.ChangeDiv}>
+                <div className={styles.MealInfo}>
+                    <div className={styles.TypeOfMeal}>{this.props.mealType}</div>
+                    <div className={styles.MealName}> {this.props.mealName}</div>
+                    <div className={styles.HoverButtons}>
+                        <a><i className="fas fa-book" title="przeczytaj przepis"></i></a>
+                        <a><i className="fas fa-retweet" title="wylosuj inną potrawę"></i></a>
+                        <a><i className="fas fa-list-ul" title="wybierz inną potrawę z listy"></i></a>
+                        <a><i className="far fa-copy" title="kopjuj"></i></a>
+                        <a><i className="fas fa-paste" title="wklej"></i></a>
+                    </div>
+                </div>
             </div>
         )
     }
