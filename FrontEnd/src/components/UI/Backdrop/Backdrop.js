d5ed1d37edb91d0212b3ea81ae11c9c47dd5c119
dianawieczorek <wieczorek.diana@outlook.com>
2019-01-27 16:21:39 +0100

    temporary remove fronted

--- FrontEnd/src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './Backdrop.css';
-
-
-const backdrop = (props) => (
-    props.show ? <div className={styles.Backdrop} onClick={props.clicked}></div> : null
-);
-
-export default backdrop;
